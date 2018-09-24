2f66d95dbf32100511f031055913c7942e2add46
Hang AmI <gradoc15@new.edvhtl.at>
2018-09-24 11:26:42 +0200

    added project

---
+++ Jg3_004_MiniExplorer/src/DateiModell.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class DateiModell extends AbstractListModel
+{
+    private ArrayList<Datei> data = new ArrayList();
+    
+    @Override
+    public int getSize()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getElementAt(int index)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
